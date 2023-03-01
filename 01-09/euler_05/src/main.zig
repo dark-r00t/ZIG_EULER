96837a78c9f8bac8cf9e5a11a226f09e00e4676f
Jan D <79925808+dark-r00t@users.noreply.github.com>
2023-03-01 00:12:57 -0500

    Update main.zig

--- 01-09/euler_05/src/main.zig
+++ 01-09/euler_05/src/main.zig
@@ -1,37 +1,47 @@
+// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
 const std = @import("std");
 
-pub fn main () !void {
+pub fn main() !void {
 
-    const start: usize = 1;
-    const end: usize = 20;
+    const answer: u32 = 232_792_560;
+    
+    const start: u32 = 1;
+    const end: u32 = 20;
 
-    var lowest: usize = 0;
-    var count: usize = end;
+    var current: u32 = end;
+    var lowest: u32 = 0;
 
-    while(true) {
-
-        var flag: bool = true;
-
-        for(range(end + 1)) |_,i| {
-            if (i < start) continue;
-
-            if(count % i != 0) {
-                flag = false;
-                break;
-            }
-        }
+    while (true) {
+        
+        var flag: bool = run(start, end, current);
 
         if (flag) {
-            lowest = count;
+            lowest = current;
             break;
         }
 
-        count += 1;
-    }
+        current += 1;
+    }   
 
-    std.debug.print("{any}\n",.{lowest});
+    std.debug.print("{any}\n",.{answer == lowest});
 }
 
-pub fn range(len: usize) []const void {
-    return @as([*]void, undefined)[0..len];
+fn run (i: u32, limit: u32, n: u32) bool {
+    
+    if (i > limit) {
+        return true;
+    }
+
+    if (n % i != 0) {
+        return false;
+    }
+
+    return (run(i + 1, limit, n));
+}
+
+// nektro
+fn range(limit: u32) []const void {
+    return @as([*]void, undefined)[0..limit];
 }
